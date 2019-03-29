3bb44785623419944cfd35f75b08b0bc0ffed2e0
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-29 15:49:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,7 @@ class Dog
     sql = <<-SQL
       SELECT id, name, breed
       FROM dogs
-      WHERE name = ?, breed = ?
+      WHERE name = ? AND breed = ?
     SQL
 
     found = DB[:conn].execute(sql, name, breed)
